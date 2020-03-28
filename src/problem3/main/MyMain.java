b6a7dfb21ebc39226c7ae7dd64b9137fa6ea8d0a
zeref-pro <61425132+zeref-pro@users.noreply.github.com>
2020-03-28 18:26:03 +0530

    MyMain.java created

--- src/problem3/main/MyMain.java
+++ src/problem3/main/MyMain.java
@@ -5,7 +5,19 @@
  *  Time: 7:13 PM
  */
 package problem3.main;
+
+import problem3.myqueue.MyPriorityQueue;
+
 // executable class
 // use problem5.student.Student class to create object of student
 public class MyMain {
+    public static void main(String[] args) {
+        MyPriorityQueue obj = new MyPriorityQueue();
+        obj.insert("tushar",1);
+        obj.insert("kartik",4);
+        obj.insert("yash",3);
+        obj.insert("pranjul",2);
+        obj.insert("jalanki",5);
+        obj.display();
+    }
 }
