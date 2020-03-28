ef8ff1292afe0be086e1aba8fad1450686d80f51
zeref-pro <61425132+zeref-pro@users.noreply.github.com>
2020-03-28 18:33:20 +0530

    Student.java created

--- src/problem5/student/Student.java
+++ src/problem5/student/Student.java
@@ -7,4 +7,34 @@
 package problem5.student;
 // to store student information and properties
 public class Student {
+    int roll;
+    int a_count;
+    int backlog;
+
+    public Student(int roll, int backlog,int a_count) {
+        this.roll = roll;
+        this.a_count = a_count;
+        this.backlog = backlog;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "roll=" + roll +
+                ", backlog=" + backlog +
+                ", a_count=" + a_count +
+                '}';
+    }
+
+    public int getRoll() {
+        return roll;
+    }
+
+    public int getA_count() {
+        return a_count;
+    }
+
+    public int getBacklog() {
+        return backlog;
+    }
 }
