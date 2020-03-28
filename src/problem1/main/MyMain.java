ce0e0d8c8c35889589d7756e09a0cec87e168032
zeref-pro <61425132+zeref-pro@users.noreply.github.com>
2020-03-28 18:19:25 +0530

    MyMain.java created

--- src/problem1/main/MyMain.java
+++ src/problem1/main/MyMain.java
@@ -7,5 +7,19 @@
 package problem1.main;
 // executable class
 
+import problem1.mybst.MyBinarySearchTree;
+
 public class MyMain {
+    public static void main(String[] args) {
+        MyBinarySearchTree obj = new MyBinarySearchTree();
+        obj.insert(5);
+        obj.insert(1);
+        obj.insert(10);
+        obj.insert(4);
+        obj.insert(6);
+        System.out.println("ALL Left Nodes");
+        obj.printLeftNode();
+        System.out.println("Count of Nodes not having left node");
+        obj.countNotLeft();
+    }
 }
