87df31fb524a57856e4c2ff2a9fa49c34fff15a5
zeref-pro <61425132+zeref-pro@users.noreply.github.com>
2020-03-28 18:24:31 +0530

    MyPriorityQueue.java

--- src/problem3/myqueue/MyPriorityQueue.java
+++ src/problem3/myqueue/MyPriorityQueue.java
@@ -6,5 +6,42 @@
  */
 package problem3.myqueue;
 
+import problem3.node.Node;
+
 public class MyPriorityQueue {
+    Node head;
+    public MyPriorityQueue(){
+        head = null;
+    }
+
+    public void insert(String name,int roll){
+        Node ns = new Node();
+        ns.setName(name);
+        ns.setRoll(roll);
+        Node temp = head;
+        if(head==null){
+            head = ns;
+        }
+        else{
+            if(head.getRoll()>roll){
+                ns.setNext(head);
+                head = temp;
+            }
+            else{
+                while(temp.getNext()!=null && temp.getNext().getRoll()<roll){
+                    temp = temp.getNext();
+                }
+                ns.setNext(temp.getNext());
+                temp.setNext(ns);
+            }
+        }
+    }
+
+    public void display(){
+        Node temp = head;
+        while(temp!=null){
+            System.out.println(temp.getName()+"\t"+temp.getRoll());
+            temp = temp.getNext();
+        }
+    }
 }
