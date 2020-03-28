19eb56a5975b675a72fb83011384b98dc4c973de
zeref-pro <61425132+zeref-pro@users.noreply.github.com>
2020-03-28 18:32:04 +0530

    Node.java created

--- src/problem5/node/Node.java
+++ src/problem5/node/Node.java
@@ -5,6 +5,27 @@
  *  Time: 7:06 PM
  */
 package problem5.node;
+
+import problem5.student.Student;
+
 // to define node properties
 public class Node {
+    Student student;
+    Node next;
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public Node getNext() {
+        return next;
+    }
 }
