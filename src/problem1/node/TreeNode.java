2f08757c7549c843a77fda627a011b6b2ce9b162
zeref-pro <61425132+zeref-pro@users.noreply.github.com>
2020-03-28 18:16:00 +0530

    tree node created

--- src/problem1/node/TreeNode.java
+++ src/problem1/node/TreeNode.java
@@ -7,4 +7,31 @@
 package problem1.node;
 
 public class TreeNode {
+    private int data;
+    private TreeNode left;
+    private TreeNode right;
+
+    public void setdata(int data){
+        this.data = data;
+    }
+
+    public int getdata(){
+        return data;
+    }
+
+    public void setRight(TreeNode right){
+        this.right = right;
+    }
+
+    public void setLeft(TreeNode left){
+        this.left = left;
+    }
+
+    public TreeNode getLeft(){
+        return left;
+    }
+
+    public TreeNode getRight(){
+        return right;
+    }
 }
