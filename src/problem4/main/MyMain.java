27d1afc890dc7eaa789f251b490ed07c65de84eb
zeref-pro <61425132+zeref-pro@users.noreply.github.com>
2020-03-28 18:29:59 +0530

    MyMain.java created

--- src/problem4/main/MyMain.java
+++ src/problem4/main/MyMain.java
@@ -5,7 +5,25 @@
  *  Time: 7:08 PM
  */
 package problem4.main;
+
+import problem1.mybst.MyBinarySearchTree;
+import problem4.myqueue.MyQueue;
+
 // executable class
 // use problem1.mybst.MyBinarySearchTree class for creating binary tree
 public class MyMain {
+    public static void main(String[] args) {
+        MyBinarySearchTree obj = new MyBinarySearchTree();
+        obj.insert(10);
+        obj.insert(3);
+        obj.insert(30);
+        obj.insert(37);
+        obj.insert(13);
+        obj.insert(1);
+        obj.insert(7);
+
+        MyQueue mq = new MyQueue();
+        mq.preSuccessor(obj.getroot());
+        mq.getPreSucc();
+    }
 }
