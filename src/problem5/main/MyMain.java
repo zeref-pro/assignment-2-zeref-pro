b7c1334b35f37538b335444d9a90a285cf9d489d
zeref-pro <61425132+zeref-pro@users.noreply.github.com>
2020-03-28 18:36:50 +0530

    MyMain.java created

--- src/problem5/main/MyMain.java
+++ src/problem5/main/MyMain.java
@@ -5,6 +5,21 @@
  *  Time: 7:06 PM
  */
 package problem5.main;
+
+import problem5.circularqueue.MyCircularQueue;
+
 //executable class
 public class MyMain {
+    public static void main(String[] args) {
+        MyCircularQueue obj = new MyCircularQueue();
+        obj.enqueue(1,4,0);
+        obj.enqueue(2,4,1);
+        obj.enqueue(3,4,4);
+        obj.enqueue(4,2,1);
+        obj.enqueue(5,2,2);
+        obj.enqueue(6,0,0);
+        obj.removeBackLog();
+        obj.display();
+
+    }
 }
