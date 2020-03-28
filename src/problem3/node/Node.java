75f05febce65eace8a1d9f1bf8286bf7e6e2eded
zeref-pro <61425132+zeref-pro@users.noreply.github.com>
2020-03-28 18:22:56 +0530

    Node.java created

--- src/problem3/node/Node.java
+++ src/problem3/node/Node.java
@@ -6,5 +6,33 @@
  */
 package problem3.node;
 
+
 public class Node {
+    String name;
+    int roll;
+    Node next;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getRoll() {
+        return roll;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setRoll(int roll) {
+        this.roll = roll;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
 }
